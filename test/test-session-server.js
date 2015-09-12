235cff8b36ddb61dc4f300cc745da543e5688c00
John Kim <yohany@gmail.com>
2015-09-13 04:51:33 +0900

    add test codes

--- test/test-session-server.js
+++ test/test-session-server.js
@@ -12,51 +12,127 @@ var address = process.argv[2] || '127.0.0.1:8888';
 var _host = address.substr(0, address.indexOf(':'));
 var _port = Number(address.substr(address.indexOf(':') + 1));
 
+var _app = 'APP_TEST';
+
+var socketOptions = {
+  transsessionPorts: ['websocket'],
+  'force new connection': true
+};
+
+var getQueryChannel = function (_channel, _user, _server, _data) {
+
+  var query = 'A=' + _app + '&' +
+    'C=' + _channel + '&' +
+    'U=' + _user.replace(/\./g, '') + '&' +
+    'D=' + 'D';
+
+  if (_server) query = query + '&S=' + _server;
+  if (_data) query = query + '&DT=' + _data;
+
+  return query;
+
+};
+
+var getQueryGlobal = function (_user) {
+
+  var query = 'A=' + _app + '&' +
+    'U=' + _user.replace(/\./g, '') + '&' +
+    'D=' + 'D';
+
+  return query;
+
+};
+
+
+var servers = {};
+
+var globalSocket = {};
+
+var channelSocket = {};
+
 describe("Chat Server", function () {
 
-  /* Test 1 - A Single User */
-  it('Should broadcast new user once they connect', function (done) {
+  it('1-1. (james) Connecting GLOBAL SOCKET', function (done) {
 
-    var app = 'APP_TEST';
-    var channel = 'CHANNEL_TEST';
-    var user = '1USER_TEST';
+    var username = 'james';
 
-    util.get(_host, _port, '/node/' + app + '/' + channel, function (err, data) {
+    util.get(_host, _port, '/node/' + _app + '/' + username, function (err, data) {
 
-      console.log(data);
+      globalSocket['james'] = {};
 
-      var query =
-          'A=' + app + '&' +
-          'C=' + channel + '&' +
-          'U=' + user.replace(/\./g, '') + '&' +
-          'D=DEVAPP&' +
-          'S=' + data.result.server.name + '&' +
-          'DT={"user":"' + user + '"}'
-        ;
+      servers['james'] = data; // james 가 접속한 서버 정보
 
-      var socketOptions = {
-        transsessionPorts: ['websocket']
-        , 'force new connection': true
-      };
+      var query = getQueryGlobal(username);
+      globalSocket.james.io = io.connect(data.result.server.url + '/global?' + query, socketOptions);
 
-
-      console.log(data.result.server.url);
-      var channelSocket = io.connect(data.result.server.url + '/channel?' + query, socketOptions);
-
-      channelSocket.on('connect', function (data) {
-
-        setInterval(function () {
-          channelSocket.emit('send', {'NM': 'message', 'DT': {'MG': faker.lorem.sentence()}});
-        }, 1000);
+      globalSocket.james.io.on('connect', function (data) {
         done();
-        //channelSocket.emit('send2', {'NM':'message', 'DT': { 'MG' : faker.lorem.sentence() } });
-
       });
+    });
 
+  });
+
+  it('1-2. (james) Connecting CHANNEL SOCKET', function (done) {
+
+    var channel = 'CH1';
+    var username = 'james';
+    channelSocket['james'] = {};
+
+    var query = getQueryChannel(channel, username, servers['james'].result.server.name);
+    var url = servers['james'].result.server.url + '/channel?' + query;
+    console.log(url);
+    channelSocket.james.io = io.connect(url, socketOptions);
+
+    channelSocket.james.io.on('connect', function (data) {
+      channelSocket.james.io.emit('send', {'NM': 'message', 'DT': {'MG': faker.lorem.sentence()}});
+      //setInterval(function () {}, 1000);
+      done();
 
     });
 
 
   });
+
+  it('2-1. (john) Connecting CHANNEL SOCKET', function (done) {
+
+    var username = 'john';
+
+    util.get(_host, _port, '/node/' + _app + '/' + username, function (err, data) {
+
+      globalSocket['john'] = {};
+
+      servers['john'] = data; // john 이 접속한 서버 정보
+
+      var query = getQueryGlobal(username);
+      globalSocket.john.io = io.connect(data.result.server.url + '/global?' + query, socketOptions);
+
+      globalSocket.john.io.on('connect', function (data) {
+        done();
+      });
+    });
+
+  });
+
+  it('2-2. (john) Connecting CHANNEL SOCKET', function (done) {
+
+    var channel = 'CH1';
+    var username = 'john';
+    channelSocket['john'] = {};
+
+    var query = getQueryChannel(channel, username, servers['john'].result.server.name);
+    var url = servers['john'].result.server.url + '/channel?' + query;
+    console.log(url);
+    channelSocket.john.io = io.connect(url, socketOptions);
+
+    channelSocket.john.io.on('connect', function (data) {
+      channelSocket.john.io.emit('send', {'NM': 'message', 'DT': {'MG': faker.lorem.sentence()}});
+      //setInterval(function () {}, 1000);
+      done();
+
+    });
+
+
+  });
+
 
 });
