a5da1885e7a99bca20469a87ba056d556a7f0bab
John Kim <yohany@gmail.com>
2015-09-26 20:11:35 +0900

    clean  up codes for mocha test

--- test/test-chat-server.js
+++ test/test-chat-server.js
@@ -1,6 +1,7 @@
 var io = require('socket.io-client');
 var util = require('./utils');
 var faker = require('faker');
+var async = require('async');
 
 /************************
  로컬의 8000 포트의 Session 서버를 통해서 100개의 Channel 을 연결하고 메시지를 보내기
@@ -19,181 +20,127 @@ var socketOptions = {
   'force new connection': true
 };
 
-var getQueryChannel = function (_channel, _user, _server, _data) {
-
-  var query = 'A=' + _app + '&' +
-    'C=' + _channel + '&' +
-    'U=' + _user.replace(/\./g, '') + '&' +
-    'D=' + 'dev1';
-
-  if (_server) query = query + '&S=' + _server;
-  if (_data) query = query + '&DT=' + _data;
-
-  return query;
-
-};
-
-var getQueryGlobal = function (_user) {
-
-  var query = 'A=' + _app + '&' +
-    'U=' + _user.replace(/\./g, '') + '&' +
-    'D=' + 'dev1';
-
-  return query;
-
-};
-
 var users = {
   'james': {
     'A': _app,
     'D': 'dev1',
     'U': 'james',
-    'PW': 'password'
+    'PW': 'password',
+    'DT': {'name': 'James Jung', 'tel': '010-1111-2234'}
   },
   'john': {
     'A': _app,
     'D': 'dev1',
     'U': 'john',
-    'PW': 'password'
+    'PW': 'password',
+    'DT': {'name': 'John Kim', 'tel': '010-1234-5678'}
   }
 };
 
-var servers = {};
+var channel = 'CH00001';
 
+var globalServer = {};
 var globalSocket = {};
 
+var channelServer = {};
 var channelSocket = {};
 
 describe("Chat Server", function () {
 
+  it('[1] 사용자 등록(수정) ', function (done) {
 
-  it('0-1. (james) Connecting GLOBAL SOCKET', function (done) {
-
-    var param = users.james;
-    param.DT = {'name': 'James Jung', 'tel': '010-1111-2234'};
-
-    util.post(_host, _port, '/user/update', param, function (err, data) {
-
-      console.error(err, data);
-
-      if (data.status == 'ok') {
-        done();
+    async.parallel(
+      [
+        function (callback) { // # 1 users.james
+          util.post(_host, _port, '/user/update', users.james, function (err, data) {
+            if (data.status == 'ok') callback();
+          });
+        },
+        function (callback) { // # 2 users.john
+          util.post(_host, _port, '/user/update', users.john, function (err, data) {
+            if (data.status == 'ok') callback();
+          });
+        }
+      ],
+      function (err, results) {
+        if (!err) done();
       }
-    });
+    );
 
   });
-  it('0-2. (john) ', function (done) {
 
-    var param = users.john;
-    param.DT = {'name': 'John Kim', 'tel': '010-1234-5678'};
-    util.post(_host, _port, '/user/update', param, function (err, data) {
-      if (data.status == 'ok') {
-        done();
+
+  it('[2] Global Socket 연결 ', function (done) {
+
+    async.parallel(
+      [
+        function (callback) { // # 1 users.james
+          util.get(_host, _port, '/node/' + _app + '/' + users.james.U, function (err, data) {
+            globalServer['james'] = data;
+            globalSocket['james'] = {};
+            var query = 'A=' + users.james.A + '&U=' + users.james.U + '&D=' + users.james.D;
+            globalSocket.james.io = io.connect(data.result.server.url + '/global?' + query, socketOptions);
+            globalSocket.james.io.on('connect', function (data) {
+              callback();
+            });
+          });
+        },
+        function (callback) { // # 2 users.john
+          util.get(_host, _port, '/node/' + _app + '/' + users.john.U, function (err, data) {
+            globalServer['john'] = data;
+            globalSocket['john'] = {};
+            var query = 'A=' + users.john.A + '&U=' + users.john.U + '&D=' + users.john.D;
+            globalSocket.john.io = io.connect(data.result.server.url + '/global?' + query, socketOptions);
+            globalSocket.john.io.on('connect', function (data) {
+              callback();
+            });
+          });
+        }
+      ],
+      function (err, results) {
+        if (!err) done();
       }
-    });
+    );
 
   });
 
-  it('1-1. (james) ', function (done) {
 
-    var username = 'james';
+  it('[3] Channel 생성 (james) ', function (done) {
 
-    util.get(_host, _port, '/node/' + _app + '/' + username, function (err, data) {
+    // james 가 채팅방에 입장한 후 john 을 추가 한다.
+    util.get(_host, _port, '/node/' + _app + '/' + channel, function (err, data) {
+      channelServer['james'] = data;
+      channelSocket['james'] = {};
 
-      globalSocket['james'] = {};
+      var query = 'A=' + users.james.A + '&U=' + users.james.U + '&D=' + users.james.D + '&C=' + channel + '&S=' + data.result.server.name;
+      channelSocket.james.io = io.connect(data.result.server.url + '/channel?' + query, socketOptions);
 
-      servers['james'] = data; // james 가 접속한 서버 정보
-
-      var query = getQueryGlobal(username);
-      globalSocket.james.io = io.connect(data.result.server.url + '/global?' + query, socketOptions);
-
-      globalSocket.james.io.on('connect', function (data) {
-        done();
+      channelSocket.james.io.on('message', function (data) {
+        console.info(' ** MESSAGE ** ', data);
       });
 
-    });
+      channelSocket.james.io.on('connect', function (data) {
 
-  });
+        // channel 에 연결된 이후 john 을 추가 한다.
+        channelSocket.james.io.emit('join', {U: 'john'}, function (result) {
 
-  it('1-2. (james) Connecting CHANNEL SOCKET', function (done) {
+          setTimeout(function () {
+            channelSocket.james.io.emit('send', {'NM': 'message', 'DT': {'MG': faker.lorem.sentence()}});
+            done();
+          }, 1500);
 
-    var channel = 'CH1';
-    var username = 'james';
-    channelSocket['james'] = {};
+        });
 
-    var query = getQueryChannel(channel, username, servers['james'].result.server.name);
-    var url = servers['james'].result.server.url + '/channel?' + query;
-    console.log(url);
-    channelSocket.james.io = io.connect(url, socketOptions);
-
-    channelSocket.james.io.on('message', function (data) {
-      console.info('(james)', data);
-    });
-
-    channelSocket.james.io.on('connect', function (data) {
-
-      channelSocket.james.io.emit('join', {U: 'james'}, function (result) {
-        setTimeout(function () {
-          channelSocket.james.io.emit('send', {'NM': 'message', 'DT': {'MG': faker.lorem.sentence()}});
-          done();
-        }, 1500);
       });
 
+
     });
 
 
   });
 
-  it('2-1. (john) Connecting GLOBAL SOCKET', function (done) {
 
-    var username = 'john';
-
-    util.get(_host, _port, '/node/' + _app + '/' + username, function (err, data) {
-
-      globalSocket['john'] = {};
-
-      servers['john'] = data; // john 이 접속한 서버 정보
-
-      var query = getQueryGlobal(username);
-      globalSocket.john.io = io.connect(data.result.server.url + '/global?' + query, socketOptions);
-
-      globalSocket.john.io.on('connect', function (data) {
-        done();
-      });
-
-    });
-
-  });
-
-  it('2-2. (john) Connecting CHANNEL SOCKET', function (done) {
-
-    var channel = 'CH1';
-    var username = 'john';
-    channelSocket['john'] = {};
-
-    var query = getQueryChannel(channel, username, servers['john'].result.server.name);
-    var url = servers['john'].result.server.url + '/channel?' + query;
-
-    channelSocket.john.io = io.connect(url, socketOptions);
-
-    channelSocket.john.io.on('message', function (data) {
-      console.info('(john)', data);
-    });
-
-    channelSocket.john.io.on('connect', function (data) {
-
-      channelSocket.john.io.emit('join', {U: 'john'}, function (result) {
-        setTimeout(function () {
-          channelSocket.john.io.emit('send', {'NM': 'message', 'DT': {'MG': faker.lorem.sentence()}});
-          done();
-        }, 1500);
-      });
-    });
-
-  });
-
-
-  it('3-1. (james) disconnect CHANNEL SOCKET', function (done) {
+  it('[9999]. Channel Socket 끊기 (james) ', function (done) {
 
     channelSocket.james.io.disconnect();
 
@@ -203,23 +150,23 @@ describe("Chat Server", function () {
 
   });
 
+  /*
+   it('4-1. (john) send message', function (done) {
 
-  it('4-1. (john) send message', function (done) {
+   channelSocket.john.io.emit('send', {'NM': 'message', 'DT': {'NO': 1, 'MG': faker.lorem.sentence()}});
+   channelSocket.john.io.emit('send', {'NM': 'message', 'DT': {'NO': 2, 'MG': faker.lorem.sentence()}});
+   channelSocket.john.io.emit('send', {'NM': 'message', 'DT': {'NO': 3, 'MG': faker.lorem.sentence()}});
+   channelSocket.john.io.emit('send', {'NM': 'message', 'DT': {'NO': 4, 'MG': faker.lorem.sentence()}});
 
-    channelSocket.john.io.emit('send', {'NM': 'message', 'DT': {'NO': 1, 'MG': faker.lorem.sentence()}});
-    channelSocket.john.io.emit('send', {'NM': 'message', 'DT': {'NO': 2, 'MG': faker.lorem.sentence()}});
-    channelSocket.john.io.emit('send', {'NM': 'message', 'DT': {'NO': 3, 'MG': faker.lorem.sentence()}});
-    channelSocket.john.io.emit('send', {'NM': 'message', 'DT': {'NO': 4, 'MG': faker.lorem.sentence()}});
+   setTimeout(function () {
+   channelSocket.john.io.disconnect();
+   done();
+   }, 800);
 
-    setTimeout(function () {
-      channelSocket.john.io.disconnect();
-      done();
-    }, 800);
+   });
+   */
 
-  });
-
-
-  it('ending.. ', function (done) {
+  it(' [종료] ', function (done) {
 
     globalSocket.james.io.disconnect();
     globalSocket.john.io.disconnect();
